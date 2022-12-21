7565d7741903d09c8d915164db806779e8ad5f33
Jake Casartelli <casartellijake@yahoo.com>
2022-12-21 16:12:40 -0500

    added controllers

---
+++ controllers/event_controller.js
@@ -0,0 +1,79 @@
+// DEPENDENCIES
+const events = require('express').Router()
+const db = require('../models')
+const { Event } = db 
+const { Op } = require('sequelize')
+
+// FIND ALL EVENTS
+events.get('/', async (req, res) => {
+    try {
+        const foundEvent = await Event.findAll()
+        
+        res.status(200).json(foundEvent)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// FIND A SPECIFIC EVENT
+events.get('/:id', async (req, res) => {
+    try {
+        const foundEvent = await Event.findOne({
+            where: { event_id: req.params.id }
+        })
+        res.status(200).json(foundEvent)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// CREATE A BAND
+events.post('/', async (req, res) => {
+    try {
+        const newEvent = await Event.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new event',
+            data: newEvent
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// UPDATE AN EVENT
+events.put('/:id', async (req, res) => {
+    try {
+        const updatedEvent = await Event.update(req.body, {
+            where: {
+                event_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedEvent} event(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE AN Event
+events.delete('/:id', async (req, res) => {
+    try {
+        const deletedEvent = await Event.destroy({
+            where: {
+                event_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedEvent} event(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+
+
+
+// EXPORT
+module.exports = events
--- server.js
+++ server.js
@@ -15,6 +15,16 @@ app.get('/', (req, res) => {
     })
 })
 
+// CONTROLLERS 
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
+const eventController = require('./controllers/event_controller')
+app.use('/events', eventController)
+
+const stageController = require('./controllers/stage_controller')
+app.use('/stages', stageController)
+
 
 // LISTEN
 app.listen(process.env.PORT, () => {
